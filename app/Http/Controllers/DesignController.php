e19e07f81b0b409cb767ff9663aaf7c1a2b3e6e8
Devjyoti karmakar <devjyoti1karmakar@gmail.com>
2025-08-08 11:41:06 +0530

    modifyed_from and selection view and the tableview uploaded

---
+++ app/Http/Controllers/DesignController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DesignController extends Controller
+{
+    public function __construct( ) {
+
+    }
+
+    public function tableDesign()
+    {
+        // dd('bhjcv');
+        return view('DesignPages.tableview');
+    }
+     public function selectionDesign()
+    {
+        // dd('bhjcv');
+        return view('DesignPages.selectionview');
+    }
+}
