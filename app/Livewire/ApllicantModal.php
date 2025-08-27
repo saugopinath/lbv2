348498abca3b8c95b8c9e36c8ef8a39e5868ae5c
shubharay21 <shubharay21@gmail.com>
2025-08-27 14:28:56 +0530

    feat: modal not open

--- app/Livewire/ApllicantModal.php
+++ app/Livewire/ApllicantModal.php
@@ -6,18 +6,19 @@ use Livewire\Component;
 
 class ApllicantModal extends Component
 {
-    public bool $openModal = false;
-    public $application_id;
-    protected $listeners = ['selfDec' => 'open'];
-    public function mount($application_id)
+    // public bool $openModal = false;
+    public $application_id, $openModal;
+    // protected $listeners = ['selfDec' => 'open'];
+    public function mount($application_id, $openModal)
     {
         $this->application_id = $application_id;
+        $this->openModal = $openModal;
     }
 
-    public function open()
-    {
-        $this->openModal = true;
-    }
+    // public function open()
+    // {
+    //     $this->openModal = true;
+    // }
 
     public function close()
     {
