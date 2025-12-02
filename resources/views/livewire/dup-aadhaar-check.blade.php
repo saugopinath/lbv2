548d29d89083349b73447f53fbf6c8f52d23527a
shubharay21 <shubharay21@gmail.com>
2025-12-02 18:56:29 +0530

    feat:wip

--- resources/views/livewire/dup-aadhaar-check.blade.php
+++ resources/views/livewire/dup-aadhaar-check.blade.php
@@ -53,39 +53,40 @@
         dsMark = false;
         dsData = false;
         Livewire.dispatch('aadhaarCheckedReset');
-    })"
-    class="grid gap-6 md:grid-cols-3 mb-6 p-4 border-b border-gray-200 dark:border-gray-700">
-    <div>
-        <x-form.input id="check_aadhar" name="aadhar_number" label="Aadhaar Number" placeholder="Enter Aadhaar Number"
-            required x-model="aadhaar" x-on:input="aadhaar = $el.value.replace(/[^0-9]/g, '').slice(0,12);
+    })">
+    <div class="grid gap-6 md:grid-cols-3 mb-6 p-4 border-b border-gray-200 dark:border-gray-700">
+        <div>
+            <x-form.input id="check_aadhar" name="aadhar_number" label="Aadhaar Number" placeholder="Enter Aadhaar Number"
+                required x-model="aadhaar" x-on:input="aadhaar = $el.value.replace(/[^0-9]/g, '').slice(0,12);
         $el.value = aadhaar;" />
+        </div>
+        <div class="flex items-end">
+            <x-button.gradient-button type="button" @click="validateAndSubmit()" wire:loading.attr="disabled"
+                wire:target="checkDuplicate" x-bind:disabled="disableCheckBtn">
+                <span wire:loading.remove wire:target="checkDuplicate">Check Availability</span>
+                <span wire:loading wire:target="checkDuplicate">Checking…</span>
+            </x-button.gradient-button>
+        </div>
+        <template x-if="successMessage">
+            <div class="mt-8 text-green-600 text-sm" x-text="successMessage"></div>
+        </template>
+        <template x-if="errorMessage">
+            <div class="mt-8 text-red-600 text-sm" x-text="errorMessage"></div>
+        </template>
+        <template x-if="findDupdicateBtn">
+            <x-button.gradient-button type="button" @click="FindDuplicate()">
+                <span>Find Duplicate</span>
+            </x-button.gradient-button>
+        </template>
+        <template x-if="dsMark">
+            <x-button.gradient-button type="button" @click="DsMark()">
+                <span>Ds Mark</span>
+            </x-button.gradient-button>
+        </template>
     </div>
-    <div class="flex items-end">
-        <x-button.gradient-button type="button" @click="validateAndSubmit()" wire:loading.attr="disabled"
-            wire:target="checkDuplicate" x-bind:disabled="disableCheckBtn">
-            <span wire:loading.remove wire:target="checkDuplicate">Check Availability</span>
-            <span wire:loading wire:target="checkDuplicate">Checking…</span>
-        </x-button.gradient-button>
+    <div x-show="dsData" x-transition>
+        <livewire:duplicate-applicant-data-table />
     </div>
-    <template x-if="errorMessage">
-        <div class="mt-8 text-red-600 text-sm" x-text="errorMessage"></div>
-    </template>
-    <template x-if="findDupdicateBtn">
-        <x-button.gradient-button type="button" @click="FindDuplicate()">
-            <span>Find Duplicate</span>
-        </x-button.gradient-button>
-    </template>
-    <template x-if="dsMark">
-        <x-button.gradient-button type="button" @click="DsMark()">
-            <span>Ds Mark</span>
-        </x-button.gradient-button>
-    </template>
-    <div x-show="dsData" x-transition class="mt-8 text-green-600 text-sm">
-        HI
-    </div>
-    <template x-if="successMessage">
-        <div class="mt-8 text-green-600 text-sm" x-text="successMessage"></div>
-    </template>
 </div>
 @push('scripts')
 <script src="{{ asset('js/adhar-verhoeff.js') }}"></script>
