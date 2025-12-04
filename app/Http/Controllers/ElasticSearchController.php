17e630d15bf82d46b5431cf559714ed945e9d8c5
shubharay21 <shubharay21@gmail.com>
2025-12-04 12:09:54 +0530

    feat:wip

--- app/Http/Controllers/ElasticSearchController.php
+++ app/Http/Controllers/ElasticSearchController.php
@@ -14,22 +14,16 @@ class ElasticSearchController extends Controller
     
     public function index()
     {
-         DB::beginTransaction();
-         $accept_reject_info = AcceptRejectInfo::create([
-                'application_id' => 150000200,
-                'beneficiary_id' => 700000200,
-                'user_id' =>8,
-                'op_type' =>21,
-                'created_at' => Carbon::now(),
-            ]);
+        
          $indexName='accept_reject_infos';
-         $elasticsearchService=new ElasticsearchService();
+         $elasticsearchService1=new ElasticsearchService();
          //dd($elasticsearchService->verifyIndex($indexName));
-         if(!$elasticsearchService->verifyIndex($indexName)){
+         /*if(!$elasticsearchService->verifyIndex($indexName)){
            $elasticsearchService->createIndex($indexName);
          }
-         $elasticsearchService->populateIndex($indexName, $accept_reject_info);
-         dd($elasticsearchService->getPaginatedIndexData($indexName));
+         $elasticsearchService->populateIndex($indexName, $accept_reject_info);*/
+       //  $elasticsearchService->populateIndex($indexName, $accept_reject_info);
+         dd($elasticsearchService1->getPaginatedIndexData($indexName));
        
     }
 }
