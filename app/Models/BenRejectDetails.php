5a796d32a8f9f9d74ec60902092d1363195aba6f
Devjyoti karmakar <devjyoti1karmakar@gmail.com>
2025-12-09 11:28:41 +0530

    fix reject nextleverole id

--- app/Models/BenRejectDetails.php
+++ app/Models/BenRejectDetails.php
@@ -60,16 +60,18 @@ class BenRejectDetails extends Model implements Auditable
     protected static function booted()
     {
         static::created(function ($benrej) {
-            // dd($benrej->application_id);
+            $nextLevelRoleId = $benrej->personal_details[0]['next_level_role_id'] ?? null;
+            // dd($nextLevelRoleId);
             $commonList = BeneficiaryCommonList::find($benrej->application_id);
+            // dd($benrej);
             // dd( get_class($benrej));
             if ($commonList) {
                 $commonList->update([
-                'sourceable_type' => get_class($benrej),
-                'is_reject'       => true,
-                'next_level_role_id'=> $benrej->next_level_role_id,
+                    'sourceable_type' => get_class($benrej),
+                    'is_reject'       => true,
+                    'next_level_role_id' => $nextLevelRoleId,
 
-            ]);
+                ]);
             }
         });
     }
