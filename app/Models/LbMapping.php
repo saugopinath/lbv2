b3fb73cf3cb9cb16f5b473d54661ef2e85ad1f41
Sanjoy Mujuri <sanjoymujuri@gmail.com>
2025-12-11 13:26:56 +0530

    wip

--- app/Models/LbMapping.php
+++ app/Models/LbMapping.php
@@ -3,9 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use OwenIt\Auditing\Contracts\Auditable;
 
-class LbMapping extends Model
+class LbMapping extends Model implements Auditable
 {
+    use \OwenIt\Auditing\Auditable;
     protected $table = 'jnmp.lb_mapping';
 
     protected $fillable = [
