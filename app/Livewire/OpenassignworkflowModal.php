1c8912371fb939de3d970c7104412ae4cc1fd86f
shubharay21 <shubharay21@gmail.com>
2025-12-18 18:06:39 +0530

    feat:wip

--- app/Livewire/OpenassignworkflowModal.php
+++ app/Livewire/OpenassignworkflowModal.php
@@ -4,25 +4,44 @@ namespace App\Livewire;
 
 use App\Models\Role;
 use App\Models\WorkflowStep;
+use App\Models\workflowstepRolemapping;
 use Livewire\Component;
 use Livewire\Attributes\On;
 
 class OpenassignworkflowModal extends Component
 {
     public bool $isOpen = false;
-    public $roles,$name;
+    public $workflowStep, $roles, $name, $selectedRoles = [];
 
     #[On('openassignworkflowModal')]
     public function assignWorkflow($id)
     {
-        $this->name = WorkflowStep::find($id)->label;
+        $this->workflowStep = WorkflowStep::find($id);
+        $this->name = $this->workflowStep->label;
         $this->isOpen = true;
         $this->roles = Role::all();
-       
     }
-     public function close()
+    public function close()
     {
         $this->isOpen = false;
+    }
+
+    public function save()
+    {
+        $this->validate([
+            'selectedRoles' => 'required|array|min:1',
+        ]);
+        $ids = $this->selectedRoles;
+        foreach ($ids as $id) {
+            $step = new workflowstepRolemapping();
+            $step->rank = $this->workflowStep->rank;
+            $step->workflow_step_id = $this->workflowStep->id;
+            $step->role_id = (int)$id;
+            $step->same_label_role_id = -$this->workflowStep->scheme_id;
+            $step->next_label_role_id = 2;
+            dd($this->workflowStep, $step);
+            // $step->save();
+        }
     }
 
     public function render()
