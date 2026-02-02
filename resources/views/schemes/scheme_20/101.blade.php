2352c2d8852667295c617def3f3d8bbe020f8677
Sanjoy Mujuri <sanjoymujuri@gmail.com>
2026-02-02 12:26:02 +0530

    wip

--- resources/views/schemes/scheme_20/101.blade.php
+++ resources/views/schemes/scheme_20/101.blade.php
@@ -1,23 +1,19 @@
 <div class="grid md:grid-cols-3 gap-4 mt-4">
 <div   >
-    <x-form.select
-    name="app_type"
-    label="Application Type"
-    
-    wire:model.live="formData.app_type"
->
-    <option value="">-- Select Application Type --</option>
-    <option value="0">Normal Entry</option>
-<option value="1">Duare Sarkar</option>
-
-</x-form.select>
-</div><div   >
     <x-form.input
     type="date"
     name="app_date"
     label="Application Date"
     
     wire:model.live="formData.app_date"
+/>
+</div><div   >
+    <x-form.input
+    type="text"
+    name="mobile_no"
+    label="Mobile Number"
+    
+    wire:model.live="formData.mobile_no"
 />
 </div><div   >
     <x-form.input
@@ -58,22 +54,14 @@
 </div><div   >
     <x-form.input
     type="text"
-    name="reg_no"
-    label="Duare Sarkar Registration Number"
-    
-    wire:model.live="formData.reg_no"
-/>
-</div></div>
-<div class="grid md:grid-cols-2 gap-4 mt-4">
-<div   >
-    <x-form.input
-    type="text"
     name="full_name"
     label="Applicant Name"
     
     wire:model.live="formData.full_name"
 />
-</div><div   >
+</div></div>
+<div class="grid md:grid-cols-3 gap-4 mt-4">
+<div   >
     <x-form.input
     type="text"
     name="email_id"
@@ -81,9 +69,7 @@
     
     wire:model.live="formData.email_id"
 />
-</div></div>
-<div class="grid md:grid-cols-3 gap-4 mt-4">
-<div   >
+</div><div   >
     <x-form.input
     type="text"
     name="ffname"
@@ -93,38 +79,25 @@
 />
 </div><div   >
     <x-form.select
-    name="caste"
-    label="Caste"
+    name="app_type"
+    label="Application Type"
     
-    wire:model.live="formData.caste"
+    wire:model.live="formData.app_type"
 >
-    <option value="">-- Select Caste --</option>
-    <option value="1">SC</option>
-<option value="2">ST</option>
-<option value="3">OBC</option>
-<option value="4">General</option>
+    <option value="">-- Select Application Type --</option>
+    <option value="0">Normal Entry</option>
+<option value="1">Duare Sarkar</option>
 
 </x-form.select>
-</div><div x-data="{
-    formData: @entangle('formData').live,
-    visible: false,
-    sync() {
-        this.visible = ['1','2','3'].includes(String(this.formData.caste));
-        if (!this.visible) {
-            this.formData.cas_cer_no = null;
-        }
-    },
-    init() {
-        this.sync();
-        this.$watch('formData.caste', () => this.sync());
-    }
-}" x-show="visible" x-cloak>
+</div></div>
+<div class="grid md:grid-cols-1 gap-4 mt-4">
+<div   >
     <x-form.input
     type="text"
-    name="cas_cer_no"
-    label="Caste Certificate Number"
+    name="reg_no"
+    label="Duare Sarkar Registration Number"
     
-    wire:model.live="formData.cas_cer_no"
+    wire:model.live="formData.reg_no"
 />
 </div></div>
 <div class="grid md:grid-cols-3 gap-4 mt-4">
@@ -143,11 +116,22 @@
 <option value="5">Widower</option>
 
 </x-form.select>
-</div><div x-data="{
-    formData: @entangle('formData').live,
-    visible: false,
-    sync() {
-        this.visible = ['2','3','5'].includes(String(this.formData.mar_statu));
+</div><div   >
+    <x-form.select
+    name="caste"
+    label="Caste"
+    
+    wire:model.live="formData.caste"
+>
+    <option value="">-- Select Caste --</option>
+    <option value="1">SC</option>
+<option value="2">ST</option>
+<option value="3">OBC</option>
+<option value="4">General</option>
+
+</x-form.select>
+</div><div x-data="{formData: @entangle('formData').live,visible: false,
+    sync() {this.visible = ['2','3','5'].includes(String(this.formData.mar_statu));
         if (!this.visible) {
             this.formData.sfname = null;
         }
@@ -163,5 +147,25 @@
     label="Spouse's Name"
     
     wire:model.live="formData.sfname"
+/>
+</div></div>
+<div class="grid md:grid-cols-1 gap-4 mt-4">
+<div x-data="{formData: @entangle('formData').live,visible: false,
+    sync() {this.visible = ['1','2','3'].includes(String(this.formData.caste));
+        if (!this.visible) {
+            this.formData.cas_cer_no = null;
+        }
+    },
+    init() {
+        this.sync();
+        this.$watch('formData.caste', () => this.sync());
+    }
+}" x-show="visible" x-cloak>
+    <x-form.input
+    type="text"
+    name="cas_cer_no"
+    label="Caste Certificate Number"
+    
+    wire:model.live="formData.cas_cer_no"
 />
 </div></div>
--- resources/views/schemes/scheme_20/102.blade.php
+++ resources/views/schemes/scheme_20/102.blade.php
@@ -1,9 +1,17 @@
 <div class="grid md:grid-cols-3 gap-4 mt-4">
 <div   >
+    <x-form.input
+    type="text"
+    name="state"
+    label="State"
+    
+    wire:model.live="formData.state"
+/>
+</div><div   >
     <x-form.select
     name="district_id"
     label="District"
-    wire:ignore
+    
     wire:model.live="formData.district_id"
 >
     <option value="">-- Select District --</option>
@@ -13,44 +21,52 @@
     <x-form.select
     name="rural_urban"
     label="Rural/Urbar"
-    wire:ignore
+    
     wire:model.live="formData.rural_urban"
 >
     <option value="">-- Select Rural/Urbar --</option>
     
 </x-form.select>
-</div><div   >
+</div></div>
+<div class="grid md:grid-cols-3 gap-4 mt-4">
+<div   >
     <x-form.select
     name="blockurban"
     label="Block/Municipality"
-    wire:ignore
+    
     wire:model.live="formData.blockurban"
 >
     <option value="">-- Select Block/Municipality --</option>
     
 </x-form.select>
-</div></div>
-<div class="grid md:grid-cols-1 gap-4 mt-4">
-<div   >
-    <x-form.input
-    type="text"
-    name="state"
-    label="State"
-    
-    wire:model.live="formData.state"
-/>
-</div></div>
-<div class="grid md:grid-cols-3 gap-4 mt-4">
-<div   >
+</div><div   >
     <x-form.select
     name="gpWard"
     label="GP / Ward"
-    wire:ignore
+    
     wire:model.live="formData.gpWard"
 >
     <option value="">-- Select GP / Ward --</option>
     
 </x-form.select>
+</div><div   >
+    <x-form.input
+    type="text"
+    name="villtowncity"
+    label="Village / Town / City"
+    
+    wire:model.live="formData.villtowncity"
+/>
+</div></div>
+<div class="grid md:grid-cols-3 gap-4 mt-4">
+<div   >
+    <x-form.input
+    type="text"
+    name="postoffice"
+    label="Post Office"
+    
+    wire:model.live="formData.postoffice"
+/>
 </div><div   >
     <x-form.input
     type="text"
@@ -76,31 +92,5 @@
     label="Pin Code"
     
     wire:model.live="formData.pincode"
-/>
-</div><div   >
-    <x-form.input
-    type="text"
-    name="villtowncity"
-    label="Village / Town / City"
-    
-    wire:model.live="formData.villtowncity"
-/>
-</div><div   >
-    <x-form.input
-    type="text"
-    name="postoffice"
-    label="Post Office"
-    
-    wire:model.live="formData.postoffice"
-/>
-</div></div>
-<div class="grid md:grid-cols-3 gap-4 mt-4">
-<div   >
-    <x-form.input
-    type="text"
-    name="mobile_no"
-    label="Mobile Number"
-    
-    wire:model.live="formData.mobile_no"
 />
 </div></div>
