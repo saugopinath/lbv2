12c226d22272c0e70c5f86b62586c6bf3299ef0b
Sanjoy Mujuri <sanjoymujuri@gmail.com>
2026-02-03 17:22:56 +0530

    wip

---
+++ resources/views/schemes/scheme_20/105.blade.php
@@ -0,0 +1,11 @@
+<div class="mt-4 space-y-3">
+
+<div class="flex items-start gap-2">
+    <x-form.checkbox name="test" value="1" label="test" wire:model="formData.test"
+    />
+</div>
+<div class="flex items-start gap-2">
+    <x-form.checkbox name="fgd" value="1" label="dfhd" wire:model="formData.fgd"
+    />
+</div>
+</div>
