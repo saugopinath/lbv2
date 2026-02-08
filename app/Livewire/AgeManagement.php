678d24ca93f1617b731277d36ba09b6ca7218192
shubharay21 <shubharay21@gmail.com>
2026-02-08 20:25:08 +0530

    feat:wip

--- app/Livewire/AgeManagement.php
+++ app/Livewire/AgeManagement.php
@@ -8,34 +8,86 @@ use Exception;
 
 class AgeManagement extends Component
 {
-    public $schemeId, $minage, $maxage, $isspecial = 'no', $specialcaseOptions = [], $specialcase;
+    public $schemeId;
+    public $minage, $maxage;
+    public $isspecial = 'no';
+    public $specialcaseOptions = [];
+    public $selectedSpecialCases = [];
+
     public function mount($schemeId)
     {
+        $this->schemeId = $schemeId;
         $this->specialcaseOptions = collect([
-            '1' => 'Farmer',
-            '2'  => 'Widow',
+            '1' => 'Handicapped',
+            '2' => 'Widow',
         ])->map(function ($name, $id) {
             return (object) ['id' => $id, 'name' => $name];
         });
-        $this->schemeId = $schemeId;
     }
+
+    public function getAvailableOptions($currentIndex)
+    {
+        $selectedIds = collect($this->selectedSpecialCases)
+            ->forget($currentIndex)
+            ->pluck('case_id')
+            ->filter()
+            ->values()
+            ->toArray();
+        return $this->specialcaseOptions->filter(function ($option) use ($selectedIds) {
+            return !in_array($option->id, $selectedIds);
+        });
+    }
+
+    public function addSpecialCase()
+    {
+        if (count($this->selectedSpecialCases) < $this->specialcaseOptions->count()) {
+            $this->selectedSpecialCases[] = [
+                'case_id' => '',
+                'min' => '',
+                'max' => ''
+            ];
+        } else {
+            $this->dispatch('toastr', ['type' => 'warning', 'message' => 'All special cases already added!']);
+        }
+    }
+
+    public function removeSpecialCase($index)
+    {
+        unset($this->selectedSpecialCases[$index]);
+        $this->selectedSpecialCases = array_values($this->selectedSpecialCases);
+    }
+
     public function save()
     {
-        $validated = $this->validate([
+        $rules = [
             'minage' => 'required|integer',
             'maxage' => 'required|integer',
-        ]);
+        ];
+
+        if ($this->isspecial === 'yes') {
+            $rules['selectedSpecialCases.*.case_id'] = 'required|distinct';
+            $rules['selectedSpecialCases.*.min'] = 'required|integer';
+            $rules['selectedSpecialCases.*.max'] = 'required|integer';
+        }
+        $customMessages = [
+            'minage.*' => 'General Min Age is Required',
+            'maxage.*' => 'General Max Age is Required',
+            'selectedSpecialCases.*.case_id.*' => 'Please Choose a Value',
+            'selectedSpecialCases.*.min.*' => 'Special Min Age is Required',
+            'selectedSpecialCases.*.max.*' => 'Special Max Age is Required',
+        ];
+        $this->validate($rules, $customMessages);
+
         DB::beginTransaction();
         try {
             DB::commit();
+            $this->dispatch('toastr', ['type' => 'success', 'message' => 'Saved Successfully!']);
         } catch (Exception $e) {
             DB::rollBack();
-            $this->dispatch('toastr', [
-                'type' => 'error',
-                'message' => 'Something went wrong: ' . $e->getMessage()
-            ]);
+            $this->dispatch('toastr', ['type' => 'error', 'message' => $e->getMessage()]);
         }
     }
+
     public function render()
     {
         return view('livewire.age-management');
