8b6390872196b9d8d462c19c6da10462fe40db10
Devjyoti2024 <devjyoti1karmakar@gmail.com>
2026-02-12 15:03:57 +0530

    user log details

--- app/Traits/WithLiveValidation.php
+++ app/Traits/WithLiveValidation.php
@@ -4,9 +4,6 @@ namespace App\Traits;
 
 trait WithLiveValidation
 {
-    /**
-     * Automatically validate a field when it is updated.
-     */
     public function updated($propertyName)
     {
         $this->validateOnly($propertyName);
