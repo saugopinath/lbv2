aaf87d643a3464199c57c5cc1166af226c992f38
Shubhadeep Ray <shubharay21@gmail.com>
2026-02-20 13:17:53 +0530

    feat:wip

--- app/Livewire/Filterlgdmasternew.php
+++ app/Livewire/Filterlgdmasternew.php
@@ -8,7 +8,8 @@ use Livewire\Component;
 class Filterlgdmasternew extends Component
 {
     public $formData = [];
-    public $visible = [
+    public $visible = [];
+    protected $defaultVisible = [
         'district_dropdown' => 0,
         'rural_urban_dropdown' => 0,
         'subdivision_dropdown' => 0,
@@ -18,104 +19,47 @@ class Filterlgdmasternew extends Component
     ];
     public function mount()
     {
-        $this->initializeForm();
+        $this->resetFilters();
+    }
+    private function applySessionLogic()
+    {
         $session = session('lgd_session');
         if (!$session) return;
         try {
             $login_type = Crypt::decryptString($session['office_type_id']);
-            if (!empty($session['district_id'])) {
-                $this->formData['district_id'] = Crypt::decryptString($session['district_id']);
+            foreach (['district_id' => 'district_id', 'block_id' => 'blockurban'] as $key => $target) {
+                if (!empty($session[$key])) $this->formData[$target] = Crypt::decryptString($session[$key]);
             }
-            if (!empty($session['block_id'])) {
-                $this->formData['blockurban'] = Crypt::decryptString($session['block_id']);
-            }
-            if ($login_type === '151') {
-                $this->visible['district_dropdown'] = 1;
-                $this->visible['assembly_dropdown'] = 1;
-                $this->visible['rural_urban_dropdown'] = 1;
-                $this->visible['block_dropdown'] = 1;
-                $this->visible['gp_ward_dropdown'] = 1;
-            }
-            if ($login_type === '152') {
-                $this->visible['assembly_dropdown'] = 1;
-                $this->visible['rural_urban_dropdown'] = 1;
-                $this->visible['block_dropdown'] = 1;
-                $this->visible['gp_ward_dropdown'] = 1;
-            }
-            if ($login_type === '153') {
-                $this->formData['rural_urban'] = 2;
-                $this->visible['gp_ward_dropdown'] = 1;
-            }
-            if ($login_type === '154') {
-                $this->visible['block_dropdown'] = 1;
-                $this->visible['gp_ward_dropdown'] = 1;
-                $this->formData['rural_urban'] = 1;
+            switch ($login_type) {
+                case '151':
+                    $this->visible = array_merge($this->visible, ['district_dropdown' => 1, 'assembly_dropdown' => 1, 'rural_urban_dropdown' => 1, 'block_dropdown' => 1, 'gp_ward_dropdown' => 1]);
+                    break;
+                case '152':
+                    $this->visible = array_merge($this->visible, ['assembly_dropdown' => 1, 'rural_urban_dropdown' => 1, 'block_dropdown' => 1, 'gp_ward_dropdown' => 1]);
+                    break;
+                case '153':
+                    $this->formData['rural_urban'] = 2;
+                    $this->visible['gp_ward_dropdown'] = 1;
+                    break;
+                case '154':
+                    $this->formData['rural_urban'] = 1;
+                    $this->visible = array_merge($this->visible, ['block_dropdown' => 1, 'gp_ward_dropdown' => 1]);
+                    break;
             }
         } catch (\Exception $e) {
-            report($e);
+            // dd($e);
         }
-    }
-    private function initializeForm()
-    {
-        $this->formData = [
-            'district_id' => '',
-            'assemblie' => '',
-            'rural_urban' => '',
-            'blockurban' => '',
-            'gpward' => '',
-        ];
-    }
-    public function filterData()
-    {
-        $payload = $this->formData;
-        $this->dispatch('filter-applied', data: $payload);
     }
     public function resetFilters()
     {
-        $this->initializeForm();
-        $this->visible = [
-            'district_dropdown' => 0,
-            'rural_urban_dropdown' => 0,
-            'subdivision_dropdown' => 0,
-            'block_dropdown' => 0,
-            'gp_ward_dropdown' => 0,
-            'assembly_dropdown' => 0,
-        ];
-        $session = session('lgd_session');
-        if ($session) {
-            try {
-                $login_type = Crypt::decryptString($session['office_type_id']);
-                if ($login_type === '151') {
-                    $this->visible['district_dropdown'] = 1;
-                    $this->visible['assembly_dropdown'] = 1;
-                    $this->visible['rural_urban_dropdown'] = 1;
-                    $this->visible['block_dropdown'] = 1;
-                    $this->visible['gp_ward_dropdown'] = 1;
-                }
-                if ($login_type === '152') {
-                    $this->formData['district_id'] = Crypt::decryptString($session['district_id']);
-                    $this->visible['assembly_dropdown'] = 1;
-                    $this->visible['rural_urban_dropdown'] = 1;
-                    $this->visible['block_dropdown'] = 1;
-                    $this->visible['gp_ward_dropdown'] = 1;
-                }
-                if ($login_type === '153') {
-                    $this->formData['district_id'] = Crypt::decryptString($session['district_id']);
-                    $this->formData['blockurban'] = Crypt::decryptString($session['block_id']);
-                    $this->formData['rural_urban'] = 2;
-                    $this->visible['gp_ward_dropdown'] = 1;
-                }
-                if ($login_type === '154') {
-                    $this->formData['district_id'] = Crypt::decryptString($session['district_id']);
-                    $this->visible['block_dropdown'] = 1;
-                    $this->visible['gp_ward_dropdown'] = 1;
-                    $this->formData['rural_urban'] = 1;
-                }
-            } catch (\Exception $e) {
-                report($e);
-            }
-        }
+        $this->formData = ['district_id' => '', 'assemblie' => '', 'rural_urban' => '', 'blockurban' => '', 'gpward' => ''];
+        $this->visible = $this->defaultVisible;
+        $this->applySessionLogic();
         $this->dispatch('filter-cleared');
+    }
+    public function filterData()
+    {
+        $this->dispatch('filter-applied', data: $this->formData);
     }
     public function render()
     {
