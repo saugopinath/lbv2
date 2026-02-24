cb8ccfc95c7dad9c2663ad11f57b1f1d28ec6e3b
Devjyoti2024 <devjyoti1karmakar@gmail.com>
2026-02-24 19:15:36 +0530

    wip

--- app/Models/CasteModificationInfo.php
+++ app/Models/CasteModificationInfo.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Facades\Auth;
 use OwenIt\Auditing\Contracts\Auditable;
 
 class CasteModificationInfo extends Model implements Auditable
@@ -27,5 +28,30 @@ class CasteModificationInfo extends Model implements Auditable
     public function nextLevelRequested()
     {
         return $this->belongsTo(Codemaster::class, 'next_level_requested_id', 'id');
+    }
+    public function transformAudit(array $data): array
+    {
+        $userId = Auth::id();
+        $userRole = UserRoleSchemeOfficeMapping::where('user_id', $userId)
+            ->value('role_id');
+        $data['tags'] = class_basename($this) . '_' . $data['event'];
+        $data['session_id'] = session()->getId();
+        // $data['other_details'] = [
+        //     'updated_by_role' => $userRole,
+        //     'user_agent' => \Illuminate\Support\Facades\Request::userAgent(),
+        //     'url' => \Illuminate\Support\Facades\Request::fullUrl(),
+        //     'method' => \Illuminate\Support\Facades\Request::method(),
+        //     'referrer' => \Illuminate\Support\Facades\Request::header('referer'),
+        // ];
+        $data['other_details'] = json_encode([
+            'updated_by_role' => $userRole,
+            'user_agent' => request()->userAgent(),
+            'url' => request()->fullUrl(),
+            'method' => request()->method(),
+            'referrer' => request()->header('referer'),
+        ]);
+
+
+        return $data;
     }
 }
