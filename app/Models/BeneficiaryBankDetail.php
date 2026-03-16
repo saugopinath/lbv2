bf7a2725ea328521c99854891c81e1f6e43bce98
Devjyoti2024 <devjyoti1karmakar@gmail.com>
2026-03-16 17:03:32 +0530

    wip

--- app/Models/BeneficiaryBankDetail.php
+++ app/Models/BeneficiaryBankDetail.php
@@ -2,10 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
-
+use Laravel\Scout\Searchable;
 class BeneficiaryBankDetail extends BaseAuditableModel
 {
+    use Searchable;
     protected $table = "pension.beneficiary_banks";
     protected $primaryKey = 'application_id';
     public $incrementing = false;
@@ -13,4 +13,26 @@ class BeneficiaryBankDetail extends BaseAuditableModel
     protected $casts = [
         'other_details' => 'array',
     ];
+    public function searchableAs()
+    {
+        return 'pension_beneficiary_banks';
+    }
+    public function toSearchableArray()
+    {
+        return [
+            'scheme_id' => $this->scheme_id,
+            'application_id' => $this->application_id,
+            'beneficiary_id' => $this->beneficiary_id,
+            'ifscode' => $this->ifscode,
+            'bankname' => $this->bankname,
+            'bank_branch_name' => $this->bank_branch_name,
+            'bankaccountnumber' => $this->bankaccountnumber,
+            'other_details' => $this->other_details,
+            'is_clean' => $this->is_clean,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
+    }
+    
 }
+
--- app/Models/BeneficiaryEnclosure.php
+++ app/Models/BeneficiaryEnclosure.php
@@ -2,8 +2,11 @@
 
 namespace App\Models;
 
+use Laravel\Scout\Searchable;
+
 class BeneficiaryEnclosure extends BaseAuditableModel
 {
+    use Searchable;
     protected $table = 'pension.beneficiary_documents';
     protected $primaryKey = 'application_id';
     public $incrementing = false;
@@ -25,5 +28,26 @@ class BeneficiaryEnclosure extends BaseAuditableModel
     public function codemaster()
     {
         return $this->belongsTo(Codemaster::class, 'document_type', 'id');
+    }
+    public function searchableAs()
+    {
+        return 'pension_beneficiary_documents';
+    }
+    public function toSearchableArray()
+    {
+        return [
+            'scheme_id' => $this->scheme_id,
+            'beneficiary_id' => $this->beneficiary_id,
+            'application_id' => $this->application_id,
+            'attched_document' => $this->attched_document,
+            'document_extension' => $this->document_extension,
+            'document_mime_type' => $this->document_mime_type,
+            'document_type' => $this->document_type,
+            'created_by' => $this->created_by,
+            'tab_code' => $this->tab_code,
+            'is_clean' => $this->is_clean,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at
+        ];
     }
 }
