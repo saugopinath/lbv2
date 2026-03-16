ab29c3cc7060f8c2adff3798afdf6073dc6cd9a1
Shubhadeep Ray <shubharay21@gmail.com>
2026-03-16 11:54:29 +0530

    wip

--- app/Livewire/Form.php
+++ app/Livewire/Form.php
@@ -2,12 +2,25 @@
 
 namespace App\Livewire;
 
+use App\Models\Scheme;
 use Livewire\Component;
 use Livewire\Attributes\On;
+
 class Form extends Component
 {
     public bool $schemeData = false;
     public $schemeId, $schemeName = null;
+    public $showSchemeDropdown = true;
+    public function mount($hideSchemeDropdown = false)
+    {
+        if ($hideSchemeDropdown) {
+            $this->showSchemeDropdown = false;
+            $schemeData = Scheme::where('is_active', 1)->first();
+            $this->schemeId = $schemeData->id;
+            $this->schemeData = true;
+            $this->schemeName = $schemeData->name;
+        }
+    }
     #[On('selectedScheme')]
     public function updateschemeData($schemeData)
     {
