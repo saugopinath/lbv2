a98f8ddbb17fab6f3aea93cb9763b3163713a6ac
Sanjoy Mujuri <sanjoymujuri@gmail.com>
2026-03-18 11:11:47 +0530

    wip

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -1,3 +1,3 @@
 <x-layouts.app>
-    <livewire:beneficiary-search :selectedOption="'mobile_number'" :inputValue="'7001306347'" :displayType="'radio'" :isApproved="true"  />
+    <livewire:form />
 </x-layouts.app>
