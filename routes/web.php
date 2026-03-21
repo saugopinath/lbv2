8b0aa57bdb5f73520415acac19720584bf8f5722
shubharay21 <shubharay21@gmail.com>
2026-03-21 22:54:36 +0530

    feat:wip

--- routes/web.php
+++ routes/web.php
@@ -47,7 +47,6 @@ use App\Http\Controllers\ValidationManagerController;
 use App\Livewire\RolerankManagement;
 use App\Livewire\SchemeTabFieldManager;
 use App\Livewire\CsvSplitter;
-
 require __DIR__ . '/home.php';
 
 
@@ -250,4 +249,17 @@ Route::controller(RejectApprovedBeneficiaryController::class)->group(function ()
 Route::controller(BackFromJBController::class)->group(function () {
     Route::any('/backfromjb', 'backfromjb')->name('backfromjb');
     Route::any('/backfromjbactions', 'backfromjbactions')->name('backfromjbactions');
+});
+
+
+Route::controller(CmoController::class)->group(function () {
+    Route::any('/pullnewcmo', 'pullnewcmo')->name('pullnewcmo');
+    Route::any('/populatelbportal', 'populatelbportal')->name('populatelbportal');
+    Route::any('/cmo-grievance-workflow', 'cmogrievanceworkflow')->name('cmo-grievance-workflow');
+    // Route::any('/cmo-grievance-find/{id}', 'cmogrievancefind')->name('cmo-grievance-find');
+    Route::any('/cmo-grievance-find', 'cmogrievancefind')->name('cmo-grievance-find');
+    Route::post('/cmo-grievance-action', 'cmodetailsaction')->name('cmo-grievance-action');
+    Route::post('/cmo-grievance-search', 'cmogrievancesearch')->name('cmo-grievance-search');
+    Route::post('/map-applicant', 'mapapplicant')->name('map-applicant');
+    Route::post('/cmo-add-actions', 'addactions')->name('cmo-add-actions');
 });
