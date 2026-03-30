a07d5f82533928a74eea1b7f50784873c7db0b5b
Sanjoy Mujuri <sanjoymujuri@gmail.com>
2026-03-30 16:57:10 +0530

    wip

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -227,178 +227,259 @@ class DashboardController extends Controller
     //             compact('dashBoardVisible')
     //         );
     //     }
+    // public function index()
+    // {
+    //     $user_id = Auth::id();
+
+    //     $roleSchemeOfficeMappings =
+    //         UserRoleSchemeOfficeMapping::with('Office')
+    //             ->where('user_id', $user_id)
+    //             ->first();
+
+    //     $allMappings =
+    //         UserRoleSchemeOfficeMapping::with('Office')
+    //             ->where('user_id', $user_id)
+    //             ->get();
+
+    //     $scheme_ids = [];
+
+    //     foreach ($allMappings as $mapping) {
+
+    //         if (! is_null($mapping->scheme_id)) {
+
+    //             $scheme_ids[] =
+    //                 Crypt::encryptString(
+    //                     $mapping->scheme_id
+    //                 );
+    //         }
+    //     }
+
+    //     Session::put(
+    //         'scheme_ids',
+    //         $scheme_ids
+    //     );
+
+    //     $lgd_session = [];
+
+    //     if (! is_null($roleSchemeOfficeMappings)) {
+
+    //         if (! empty($scheme_ids)) {
+
+    //             $lgd_session['scheme_id'] =
+    //                 $scheme_ids;
+
+    //         }
+
+    //         if (! is_null($roleSchemeOfficeMappings->role_id)) {
+
+    //             $lgd_session['role_id'] =
+    //                 Crypt::encryptString(
+    //                     $roleSchemeOfficeMappings->role_id
+    //                 );
+    //         }
+
+    //         if (! is_null($roleSchemeOfficeMappings->Office)) {
+
+    //             $office =
+    //                 $roleSchemeOfficeMappings->Office;
+
+    //             if (! is_null($office->office_type_id)) {
+
+    //                 $lgd_session['office_type_id'] =
+    //                     Crypt::encryptString(
+    //                         $office->office_type_id
+    //                     );
+    //             }
+
+    //             if (! is_null($office->state_id)) {
+
+    //                 $lgd_session['state_id'] =
+    //                     Crypt::encryptString(
+    //                         $office->state_id
+    //                     );
+    //             }
+
+    //             if (! is_null($office->district_id)) {
+
+    //                 $lgd_session['district_id'] =
+    //                     Crypt::encryptString(
+    //                         $office->district_id
+    //                     );
+    //             }
+
+    //             if (! is_null($office->block_id)) {
+
+    //                 $lgd_session['block_id'] =
+    //                     Crypt::encryptString(
+    //                         $office->block_id
+    //                     );
+    //             }
+
+    //             if (! is_null($office->subdivision_id)) {
+
+    //                 $lgd_session['subdivision_id'] =
+    //                     Crypt::encryptString(
+    //                         $office->subdivision_id
+    //                     );
+    //             }
+
+    //             if (! is_null($office->panchayat_id)) {
+
+    //                 $lgd_session['panchayat_id'] =
+    //                     Crypt::encryptString(
+    //                         $office->panchayat_id
+    //                     );
+    //             }
+
+    //             if (! is_null($office->municipalitiy_id)) {
+
+    //                 $lgd_session['municipalitiy_id'] =
+    //                     Crypt::encryptString(
+    //                         $office->municipalitiy_id
+    //                     );
+    //             }
+
+    //             if (! is_null($office->ward_id)) {
+
+    //                 $lgd_session['ward_id'] =
+    //                     Crypt::encryptString(
+    //                         $office->ward_id
+    //                     );
+    //             }
+    //         }
+
+    //     } else {
+
+    //         /* Default fallback */
+
+    //         $scheme =
+    //             Scheme::where(
+    //                 'short_name',
+    //                 'LB'
+    //             )->first();
+
+    //         $lgd_session['scheme_id'] =
+    //             [
+    //                 Crypt::encryptString(
+    //                     $scheme->id
+    //                 ),
+    //             ];
+
+    //         $lgd_session['office_type_id'] =
+    //             Crypt::encryptString(151);
+
+    //         $lgd_session['state_id'] =
+    //             Crypt::encryptString(1);
+    //     }
+
+    //     /* Store session */
+
+    //     Session::put(
+    //         'lgd_session',
+    //         $lgd_session
+    //     );
+
+    //     /* Dashboard visibility */
+
+    //     $roleID =
+    //         $roleSchemeOfficeMappings->role_id ?? 0;
+
+    //     if ($roleID == 1) {
+
+    //         $dashBoardVisible = 1;
+
+    //     } else {
+
+    //         $dashBoardVisible = 0;
+    //     }
+
+    //     /* Debug if needed */
+    //     // dd( $lgd_session);
+
+    //     return view(
+    //         'admin.index',
+    //         compact('dashBoardVisible')
+    //     );
+    // }
     public function index()
     {
         $user_id = Auth::id();
-
-        $roleSchemeOfficeMappings =
-            UserRoleSchemeOfficeMapping::with('Office')
-                ->where('user_id', $user_id)
-                ->first();
-
-        $allMappings =
-            UserRoleSchemeOfficeMapping::with('Office')
-                ->where('user_id', $user_id)
-                ->get();
-
+        $allMappings = UserRoleSchemeOfficeMapping::with('Office')->where('user_id', $user_id)->get();
+        $roleSchemeOfficeMappings = $allMappings->first();
         $scheme_ids = [];
 
         foreach ($allMappings as $mapping) {
-
-            if (! is_null($mapping->scheme_id)) {
-
-                $scheme_ids[] =
-                    Crypt::encryptString(
-                        $mapping->scheme_id
-                    );
+            if (!is_null($mapping->scheme_id)) {
+                $scheme_ids[] = Crypt::encryptString($mapping->scheme_id);
             }
         }
-
-        Session::put(
-            'scheme_ids',
-            $scheme_ids
-        );
-
+        Session::put('scheme_ids', $scheme_ids);
         $lgd_session = [];
 
-        if (! is_null($roleSchemeOfficeMappings)) {
+        if (!is_null($roleSchemeOfficeMappings)) {
 
             if (! empty($scheme_ids)) {
-
-                $lgd_session['scheme_id'] =
-                    $scheme_ids;
-
+                $lgd_session['scheme_id'] = $scheme_ids;
             }
 
-            if (! is_null($roleSchemeOfficeMappings->role_id)) {
-
-                $lgd_session['role_id'] =
-                    Crypt::encryptString(
-                        $roleSchemeOfficeMappings->role_id
-                    );
+            if (!is_null($roleSchemeOfficeMappings->role_id)) {
+                $lgd_session['role_id'] = Crypt::encryptString($roleSchemeOfficeMappings->role_id);
             }
 
-            if (! is_null($roleSchemeOfficeMappings->Office)) {
+            if (!is_null($roleSchemeOfficeMappings->Office)) {
 
-                $office =
-                    $roleSchemeOfficeMappings->Office;
+                $office = $roleSchemeOfficeMappings->Office;
 
-                if (! is_null($office->office_type_id)) {
-
-                    $lgd_session['office_type_id'] =
-                        Crypt::encryptString(
-                            $office->office_type_id
-                        );
+                if (!is_null($office->office_type_id)) {
+                    $lgd_session['office_type_id'] = Crypt::encryptString($office->office_type_id);
                 }
 
-                if (! is_null($office->state_id)) {
-
-                    $lgd_session['state_id'] =
-                        Crypt::encryptString(
-                            $office->state_id
-                        );
+                if (!is_null($office->state_id)) {
+                    $lgd_session['state_id'] = Crypt::encryptString($office->state_id);
                 }
 
-                if (! is_null($office->district_id)) {
-
-                    $lgd_session['district_id'] =
-                        Crypt::encryptString(
-                            $office->district_id
-                        );
+                if (!is_null($office->district_id)) {
+                    $lgd_session['district_id'] = Crypt::encryptString($office->district_id);
                 }
 
-                if (! is_null($office->block_id)) {
-
-                    $lgd_session['block_id'] =
-                        Crypt::encryptString(
-                            $office->block_id
-                        );
+                if (!is_null($office->block_id)) {
+                    $lgd_session['block_id'] = Crypt::encryptString($office->block_id);
                 }
 
-                if (! is_null($office->subdivision_id)) {
-
-                    $lgd_session['subdivision_id'] =
-                        Crypt::encryptString(
-                            $office->subdivision_id
-                        );
+                if (!is_null($office->subdivision_id)) {
+                    $lgd_session['subdivision_id'] = Crypt::encryptString($office->subdivision_id);
                 }
 
-                if (! is_null($office->panchayat_id)) {
-
-                    $lgd_session['panchayat_id'] =
-                        Crypt::encryptString(
-                            $office->panchayat_id
-                        );
+                if (!is_null($office->panchayat_id)) {
+                    $lgd_session['panchayat_id'] = Crypt::encryptString($office->panchayat_id);
                 }
 
-                if (! is_null($office->municipalitiy_id)) {
-
-                    $lgd_session['municipalitiy_id'] =
-                        Crypt::encryptString(
-                            $office->municipalitiy_id
-                        );
+                if (!is_null($office->municipalitiy_id)) {
+                    $lgd_session['municipalitiy_id'] = Crypt::encryptString($office->municipalitiy_id);
                 }
 
-                if (! is_null($office->ward_id)) {
-
-                    $lgd_session['ward_id'] =
-                        Crypt::encryptString(
-                            $office->ward_id
-                        );
+                if (!is_null($office->ward_id)) {
+                    $lgd_session['ward_id'] = Crypt::encryptString($office->ward_id);
                 }
             }
 
         } else {
 
-            /* Default fallback */
+            $scheme = Scheme::where('short_name', 'LB')->first();
 
-            $scheme =
-                Scheme::where(
-                    'short_name',
-                    'LB'
-                )->first();
+            $lgd_session['scheme_id'] = [Crypt::encryptString($scheme->id)];
 
-            $lgd_session['scheme_id'] =
-                [
-                    Crypt::encryptString(
-                        $scheme->id
-                    ),
-                ];
+            $lgd_session['office_type_id'] = Crypt::encryptString(151);
 
-            $lgd_session['office_type_id'] =
-                Crypt::encryptString(151);
-
-            $lgd_session['state_id'] =
-                Crypt::encryptString(1);
+            $lgd_session['state_id'] = Crypt::encryptString(1);
         }
 
-        /* Store session */
+        Session::put('lgd_session', $lgd_session);
 
-        Session::put(
-            'lgd_session',
-            $lgd_session
-        );
+        $roleID = $roleSchemeOfficeMappings->role_id ?? 0;
 
-        /* Dashboard visibility */
+        $dashBoardVisible = ($roleID == 1) ? 1 : 0;
 
-        $roleID =
-            $roleSchemeOfficeMappings->role_id ?? 0;
-
-        if ($roleID == 1) {
-
-            $dashBoardVisible = 1;
-
-        } else {
-
-            $dashBoardVisible = 0;
-        }
-
-        /* Debug if needed */
-        // dd( $lgd_session);
-
-        return view(
-            'admin.index',
-            compact('dashBoardVisible')
-        );
+        return view('admin.index', compact('dashBoardVisible'));
     }
 }
