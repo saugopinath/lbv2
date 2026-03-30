79019df220b81ddd5a0255bf3091c2c08ab20ee7
Sanjoy Mujuri <sanjoymujuri@gmail.com>
2026-03-30 16:19:43 +0530

    wip

--- resources/views/components/layouts/das_side_menu.blade.php
+++ resources/views/components/layouts/das_side_menu.blade.php
@@ -432,21 +432,22 @@
         </div>
 
     @endif
-    <div>
-        <a href="{{ route('beneficiaries_selection.index') }}"
-            class="flex items-center w-full px-4 py-2 text-left hover:bg-slate-700 dark:hover:bg-slate-700 text-slate-200 hover:text-white rounded">
-            <svg class="w-5 h-5 mr-2 flex-shrink-0" xmlns="http://www.w3.org/2000/svg" fill="none"
-                viewBox="0 0 24 24" stroke="currentColor">
+    @if (\App\Helpers\WorkFlowPermissionHelper::canViewBeneficiaries())
+        <div>
+            <a href="{{ route('beneficiaries_selection.index') }}"
+                class="flex items-center w-full px-4 py-2 text-left hover:bg-slate-700 dark:hover:bg-slate-700 text-slate-200 hover:text-white rounded">
+                <svg class="w-5 h-5 mr-2 flex-shrink-0" xmlns="http://www.w3.org/2000/svg" fill="none"
+                    viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                        d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-6 9l2 2 4-4" />
+                </svg>
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                    d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-6 9l2 2 4-4" />
-            </svg>
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                d="M17 20h5v-2a2 2 0 00-2-2h-3m-2-2H7a2 2 0 01-2-2V5a2 2 0 012-2h10a2 2 0 012 2v7m-7 4v5m-7-5h12" />
-            </svg>
-            <span x-show="sidebar" class="mr-2 truncate">Beneficiary List</span>
-        </a>
-    </div>
-
+                    d="M17 20h5v-2a2 2 0 00-2-2h-3m-2-2H7a2 2 0 01-2-2V5a2 2 0 012-2h10a2 2 0 012 2v7m-7 4v5m-7-5h12" />
+                </svg>
+                <span x-show="sidebar" class="mr-2 truncate">Beneficiary List</span>
+            </a>
+        </div>
+    @endif
     @if (\App\Helpers\WorkFlowPermissionHelper::canCaste())
         <div>
             <button @click="activeMenu === 'CasteManagement' ? activeMenu = null : activeMenu = 'CasteManagement'"
