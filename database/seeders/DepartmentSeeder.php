c5305257285ea0fdbd692359953e084cb175651b
Shubhadeep Ray <shubharay21@gmail.com>
2026-05-13 15:44:32 +0530

    feat: add DepartmentSeeder and SchemeSeeder for initial database population

--- database/seeders/DepartmentSeeder.php
+++ database/seeders/DepartmentSeeder.php
@@ -14,20 +14,55 @@ class DepartmentSeeder extends Seeder
     public function run(): void
     {
         $departments = array(
-            
             array(
                 "state_code" => "19",
                 "department_name" => "Department of Women & Child Development and Social Welfare",
                 "short_name" => "WCD",
-            )
-            
+            ),
+            array(
+                "state_code" => "19",
+                "department_name" => "Department of Women & Child Development and Social Welfare",
+                "short_name" => "WCD&SWD",
+            ),
+            array(
+                "state_code" => "19",
+                "department_name" => "Tribal Welfare Department",
+                "short_name" => "TWD",
+            ),
+            array(
+                "state_code" => "19",
+                "department_name" => "Department of Fisheries",
+                "short_name" => "Fisheries",
+            ),
+            array(
+                "state_code" => "19",
+                "department_name" => "Micro, Small & Medium Enterprises and Textiles Department",
+                "short_name" => "MSME&T",
+            ),
+            array(
+                "state_code" => "19",
+                "department_name" => "Information and Cultural Affairs Department",
+                "short_name" => "I&CA",
+            ),
+            array(
+                "state_code" => "19",
+                "department_name" => "Backward Classes Welfare Department",
+                "short_name" => "BCWD",
+            ),
+            array(
+                "state_code" => "19",
+                "department_name" => "No Department",
+                "short_name" => "null",
+            ),
         );
         foreach ($departments as $department_item) {
-            Department::create([
-                'name'     => strtoupper($department_item['department_name']),
-                'short_name'     => $department_item['short_name'],
-                'state_id'   => State::where('id', '19')->firstOrFail()->id,
-            ]);
+            Department::updateOrCreate(
+                ['short_name' => $department_item['short_name']],
+                [
+                    'name'     => strtoupper($department_item['department_name']),
+                    'state_id'   => State::where('lgd_code', '19')->firstOrFail()->id,
+                ]
+            );
         }
     }
 }
