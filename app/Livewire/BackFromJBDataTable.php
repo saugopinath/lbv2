d93c69c6d92034113da1ffd8974639edb6320b16
Shubhadeep Ray <shubharay21@gmail.com>
2026-05-14 18:26:36 +0530

    feat: create BackFromJBDataTable for managing back-from-JB application workflows

--- app/Livewire/BackFromJBDataTable.php
+++ app/Livewire/BackFromJBDataTable.php
@@ -176,13 +176,26 @@ class BackFromJBDataTable extends DataTableComponent
 
     public function builder(): Builder
     {
-        $query = BackFromJb::with([
-            'beneficiary.contact'
-        ])->whereHas('beneficiary', function ($q) {
-            foreach ($this->filter_condition as $col => $val) {
-                $q->where($col, $val);
-            }
-        });
+        // $query = BackFromJb::with([
+        //     'beneficiary.contact'
+        // ])->whereHas('beneficiary', function ($q) {
+        //     foreach ($this->filter_condition as $col => $val) {
+        //         $q->where($col, $val);
+        //     }
+        // });
+
+        $query = BackFromJb::query()
+            ->select([
+                'application_id',
+                'next_level_role_id'
+            ])
+            ->with([
+                'beneficiary:application_id,beneficiary_name,other_details'
+            ])->whereHas('beneficiary', function ($q) {
+                foreach ($this->filter_condition as $col => $val) {
+                    $q->where($col, $val);
+                }
+            });
         if (!empty($this->next_level_role_id)) {
             $query->where('next_level_role_id', $this->next_level_role_id);
         }
